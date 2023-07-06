36ba8764fd399b4b6be41de5bf224956150b30b7
Grvk <vika@granadzer.com>
2023-07-06 22:00:36 +0300

    Final commmit

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ def uniqueChar(ftext):
     for word in words:
         counter = Counter(word)
         counter = sorted(counter.items(), key= lambda x:x[1])
-        print(counter)
-        print(counter[0])
-        print(counter[0][0])
         if (counter[0][0] in char_dict) and (counter[0][1] == 1):
             char_dict[counter[0][0]] += 1
         else:
             char_dict[counter[0][0]] = 1
 
-    print(char_dict)
     for element in char_dict:
         if char_dict[element] == 1:
             return element
