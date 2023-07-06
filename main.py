d430239cb6c935d69e9bccd0e9f371746978a4ec
Grvk <vika@granadzer.com>
2023-07-06 21:54:03 +0300

    Second commmit

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from collections import Counter
 
 def uniqueChar(ftext):
     words = ftext.split()
-    words = dict.fromkeys(words)
     char_dict = {}
     for word in words:
         counter = Counter(word)
         counter = sorted(counter.items(), key= lambda x:x[1])
+        print(counter)
+        print(counter[0])
+        print(counter[0][0])
         if (counter[0][0] in char_dict) and (counter[0][1] == 1):
             char_dict[counter[0][0]] += 1
         else:
             char_dict[counter[0][0]] = 1
 
+    print(char_dict)
     for element in char_dict:
         if char_dict[element] == 1:
             return element
@@ -22,4 +25,4 @@ def uniqueChar(ftext):
 
 file = open("text.txt", "r")
 text = file.read()
-print("Answer: " + uniqueChar(text.lower()))
+print("Answer: " + uniqueChar(text))
